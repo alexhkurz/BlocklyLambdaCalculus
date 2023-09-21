ca62deb1dc11feb29403e8c5c9f1f319961f35b8
Alexander Kurz <alexhkurz@gmail.com>
2023-09-20 20:14:27 -0700

    aider: Changed the color of the function definition block to make it brighter.
    # Aider chat conversation:
    
    USER: can you make it more orange, I mean brighter.
    
    ASSISTANT: Sure, to make the function definition block more orange (brighter), we need to modify the `custom_blocks.js` file. Specifically, we need to change the color of the `functionDef` block.
    
    Here is the change:
    
    ```javascript
    Definitions/custom_blocks.js
    <<<<<<< HEAD
        this.setColour(30);
    =======
        this.setColour(20);
    >>>>>>> updated
    ```
    
    This change will make the color of the function definition block brighter. The `setColour` function takes a hue value between 0 (red) and 360 (red again), with 120 (green) and 240 (blue) in between. Lowering the value from 30 to 20 will make the color more orange.

--- Definitions/custom_blocks.js
+++ Definitions/custom_blocks.js
@@ -100,7 +100,7 @@ Blockly.Blocks['functionDef'] = {
         .setCheck(["var", "exp", "Number"]);
     this.setInputsInline(true);
     this.setOutput(true, "exp");
-    this.setColour(30);
+    this.setColour(20);
     this.setTooltip("");
     this.setHelpUrl("");
   }
