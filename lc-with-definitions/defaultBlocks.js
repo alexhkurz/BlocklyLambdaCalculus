dbff16ee4dfeba828a99e0790661d55d7397da6d
Alexander Kurz <alexhkurz@gmail.com>
2023-10-15 09:03:43 -0700

    Changed the x coordinate of a block in the defaultBlocks.js file.

--- lc-with-definitions/defaultBlocks.js
+++ lc-with-definitions/defaultBlocks.js
@@ -1,1 +1,33 @@
-window.defaultBlocks = '<xml xmlns="https://developers.google.com/blockly/xml">\n<block type="app" id="|*nT426^Js?MG3aO+bXD" x="57" y="78">\n  <value name="EXPR1">\n    <block type="abs" id="7znB^lm.*76=$cfE_3rX">\n      <value name="VAR">\n        <block type="var" id="k)Q]$^YH;K77M[S%?:31">\n          <field name="NAME">x</field>\n        </block>\n      </value>\n      <value name="EXPR">\n        <block type="math_arithmetic" id="_OOsz0y!G?ZALI6*!p:0">\n          <field name="OP">ADD</field>\n          <value name="A">\n            <block type="var" id="~y[MCf2?m(5ttw;tYt+z">\n              <field name="NAME">x</field>\n            </block>\n          </value>\n          <value name="B">\n            <block type="math_number" id="sYHQvj3I3*+5jya=0}0(">\n              <field name="NUM">2</field>\n            </block>\n          </value>\n        </block>\n      </value>\n    </block>\n  </value>\n  <value name="EXPR2">\n    <block type="math_number" id="A[Y|QmJ8,)wWo6TU-[Xt">\n      <field name="NUM">1</field>\n    </block>\n  </value>\n</block>\n</xml>';
+window.defaultBlocks = '<xml xmlns="https://developers.google.com/blockly/xml">
+<block type="app" id="|*nT426^Js?MG3aO+bXD" x="68" y="78">
+  <value name="EXPR1">
+    <block type="abs" id="7znB^lm.*76=$cfE_3rX">
+      <value name="VAR">
+        <block type="var" id="k)Q]$^YH;K77M[S%?:31">
+          <field name="NAME">x</field>
+        </block>
+      </value>
+      <value name="EXPR">
+        <block type="math_arithmetic" id="_OOsz0y!G?ZALI6*!p:0">
+          <field name="OP">ADD</field>
+          <value name="A">
+            <block type="var" id="~y[MCf2?m(5ttw;tYt+z">
+              <field name="NAME">x</field>
+            </block>
+          </value>
+          <value name="B">
+            <block type="math_number" id="A[Y|QmJ8,)wWo6TU-[Xt">
+              <field name="NUM">1</field>
+            </block>
+          </value>
+        </block>
+      </value>
+    </block>
+  </value>
+  <value name="EXPR2">
+    <block type="math_number" id="sYHQvj3I3*+5jya=0}0(">
+      <field name="NUM">2</field>
+    </block>
+  </value>
+</block>
+</xml>';
