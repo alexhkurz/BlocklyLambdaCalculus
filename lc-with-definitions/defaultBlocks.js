29304d6819053447fdd7403667e526a29edb8d1d
Alexander Kurz <alexhkurz@gmail.com>
2023-10-15 08:49:21 -0700

    aider: Added /Users/akurz/alexhkurz-at-git/BlocklyLambda/lc-with-definitions/defaultBlocks.js

--- lc-with-definitions/defaultBlocks.js
+++ lc-with-definitions/defaultBlocks.js
@@ -1,1 +1,33 @@
-window.defaultBlocks = '<xml xmlns="https://developers.google.com/blockly/xml"><block type="functionDef" x="10" y="10"></block></xml>';
+window.defaultBlocks = '<xml xmlns="https://developers.google.com/blockly/xml">
+<block type="app" id="|*nT426^Js?MG3aO+bXD" x="57" y="78">
+  <value name="EXPR1">
+    <block type="abs" id="7znB^lm.*76=$cfE_3rX">
+      <value name="VAR">
+        <block type="var" id="k)Q]$^YH;K77M[S%?:31">
+          <field name="NAME">x</field>
+        </block>
+      </value>
+      <value name="EXPR">
+        <block type="math_arithmetic" id="_OOsz0y!G?ZALI6*!p:0">
+          <field name="OP">ADD</field>
+          <value name="A">
+            <block type="var" id="~y[MCf2?m(5ttw;tYt+z">
+              <field name="NAME">x</field>
+            </block>
+          </value>
+          <value name="B">
+            <block type="math_number" id="sYHQvj3I3*+5jya=0}0(">
+              <field name="NUM">2</field>
+            </block>
+          </value>
+        </block>
+      </value>
+    </block>
+  </value>
+  <value name="EXPR2">
+    <block type="math_number" id="A[Y|QmJ8,)wWo6TU-[Xt">
+      <field name="NUM">1</field>
+    </block>
+  </value>
+</block>
+</xml>';
